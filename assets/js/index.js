e2f3df16b19cb916f1eb1def9ddde0165347bb5d
Luiz Carlos Froes <luizfroes@gmail.com>
2021-10-15 01:08:46 +0100

    adding the time blocks

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,20 +7,68 @@ const timeBlockLabels = [
   { label: "2 PM", key: 14 },
   { label: "3 PM", key: 15 },
   { label: "4 PM", key: 16 },
-  { label: "5 AM", key: 17 },
+  { label: "5 PM", key: 17 },
 ];
 
 const currentDay = $("#current-day");
 const clockContainer = $("#clock");
+const timeBlockContainer = $("#time-block-container");
 
-const constructTimeBlock = function () {
-  //get text from object
+const getFromLocalStorage = function (key, defaultValue) {
+  const localStorageData = JSON.parse(localStorage.getItem(key));
+  //console.log(localStorageData);
+
+  if (!localStorageData) {
+    return defaultValue;
+  } else {
+    return localStorageData;
+  }
+};
+
+const constructTimeBlock = function (each) {
+  const currentTime = 12;
+  const isPresent = function () {
+    each.key === currentTime;
+  };
+
+  const isFuture = function () {
+    each.key > currentTime;
+  };
+
+  const timeBlock = `<div class="card-container" id="card-container">
+  <div class="hour-container">
+    <h2 class="hour">${each.label}</h2>
+  </div>
+  <div class="textarea-container">
+    <textarea class="textarea" id="textarea"></textarea>
+  </div>
+  <div class="btn-container">
+    <button class="save-btn"><i class="far fa-save"></i></i></button>
+    <button class="clear-btn"><i class="far fa-trash-alt"></i></button>
+  </div>
+</div>`;
+
+  //append to main
+  $(timeBlock).appendTo(timeBlockContainer);
+
+  //get text from LS object
+  //getFromLocalStorage();
+
   //check if present
+  if (isPresent) {
+    $("#textarea-container").toggleClass("textarea-present");
+  }
+
   //check if future
+  if (isFuture) {
+    $("#textarea-container").toggleClass("textarea-future");
+  }
+  return console.log(timeBlock);
 };
 
 const renderTimeBlocks = function () {
   //map over the timeBlockLabels[] (constructTimeBlock)
+  timeBlockLabels.map(constructTimeBlock);
 };
 
 const renderCurrentDay = function () {
@@ -53,4 +101,3 @@ const onReady = function () {
 };
 
 $(document).ready(onReady);
-console.log(clockContainer);
