4b4d1c03e70af9034be5a7ab9553323cad18b6a9
Luiz Carlos Froes <luizfroes@gmail.com>
2021-10-17 22:23:51 +0100

    debugged the code

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,122 +1,126 @@
 const timeBlockLabels = [
-  { label: "9 AM", key: 9, textInput: "" },
-  { label: "10 AM", key: 10, textInput: "" },
-  { label: "11 AM", key: 11, textInput: "" },
-  { label: "12 PM", key: 12, textInput: "" },
-  { label: "1 PM", key: 13, textInput: "" },
-  { label: "2 PM", key: 14, textInput: "" },
-  { label: "3 PM", key: 15, textInput: "" },
-  { label: "4 PM", key: 16, textInput: "" },
-  { label: "5 PM", key: 17, textInput: "" },
+  { label: "9 AM", key: 9 },
+  { label: "10 AM", key: 10 },
+  { label: "11 AM", key: 11 },
+  { label: "12 PM", key: 12 },
+  { label: "1 PM", key: 13 },
+  { label: "2 PM", key: 14 },
+  { label: "3 PM", key: 15 },
+  { label: "4 PM", key: 16 },
+  { label: "5 PM", key: 17 },
 ];
 
 const currentDay = $("#current-day");
 const clockContainer = $("#clock");
 
-const textInputArray = JSON.parse(localStorage.getItem("textInput")) || [];
+const getDataFromLS = function () {
+  return JSON.parse(localStorage.getItem("textInput")) || {};
+};
 
 const onSave = function (event) {
   const target = $(event.target);
 
-  if (target.is('button[name="save-btn"]', 'i[name="save-btn"]')) {
-    const timeData = {
-      key: target.attr("data-key"),
-      textInput: $("#textarea").val(),
-    };
+  if (
+    target.is('button[name="save-btn"]') ||
+    target.is('i[name="save-icon"]')
+  ) {
+    const data = getDataFromLS();
+    const key = target.attr("id");
 
-    //add to LS
-    textInputArray.push(timeData);
+    data[key] = $(`textarea[id="${key}"]`).val();
 
-    localStorage.setItem("textInput", JSON.stringify(textInputArray));
-    console.log(textInputArray);
-    console.log(localStorage);
+    localStorage.setItem("textInput", JSON.stringify(data));
   }
 };
 
-const constructTimeBlock = function (each) {
-  const currentTime = moment().format("HH");
+const getTextAreaValue = function (key) {
+  // get all data from LS
+  const data = getDataFromLS();
 
-  const isPast = currentTime > each.key;
-  const isFuture = currentTime < each.key;
+  return data[key];
+};
 
-  let textareaClass;
+const getClassName = function (key) {
+  const currentTime = moment().hour();
+
+  const isPast = currentTime > key;
+  const isFuture = currentTime < key;
 
   // check if it is past, future or present
   if (isPast) {
-    textareaClass = "textarea-container";
+    return "textarea-container";
   } else if (isFuture) {
-    textareaClass = "textarea-future";
+    return "textarea-future";
   } else {
-    textareaClass = "textarea-present";
+    return "textarea-present";
   }
+};
 
+const constructTimeBlocks = function (each) {
   //render timeBlock
   const timeBlock = `<div class="card-container" id="card-container">
-  <div class="hour-container">
-    <h2 class="hour">${each.label}</h2>
-  </div>
-  <div class="${textareaClass}">
-    <textarea class="textarea" id="textarea" data="${each.key}"></textarea>
-  </div>
-  <div id="btn-container" class="btn-container">
-    <button name="save-btn" id="save-btn" class="save-btn" data-key="${each.key}"><i name="save-icon" class="far fa-save" data="${each.key}"></i></i></button>
-  </div>
-</div>`;
+    <div class="hour-container">
+      <h2 class="hour">${each.label}</h2>
+    </div>
+    <div class="${getClassName(each.key)}">
+      <textarea class="textarea" id=${each.key} data="${each.key}">${
+    getTextAreaValue(each.key) || ""
+  }</textarea>
+    </div>
+    <div id="btn-container" class="btn-container">
+      <button name="save-btn" id=${
+        each.key
+      } class="save-btn"><i name="save-icon" id=${
+    each.key
+  } class="far fa-save"></i></i></button>
+    </div>
+  </div>`;
+
+  return timeBlock;
+};
+
+const renderTimeBlocks = function () {
+  //map over the timeBlockLabels to construct each TimeBlock
+  const timeBlocks = timeBlockLabels.map(constructTimeBlocks).join("");
 
   //append to main
-  $(timeBlock).appendTo("#time-block-container");
-
-  $(".hour #textarea").val(localStorage.getItem("key"));
-  // const textareaInput = function (item) {
-  //   console.log(item.key, $("#textarea").data());
-  //   // if (item.key === $("#textarea").data()) {
-  //   //   $("#textarea").val(item.textInput);
-  //   //   console.log(item.key, $("#textarea").data());
-  //   //   return;
-  //   //}
-  // };
-
-  // textInputArray.map(textareaInput);
+  $("#time-block-container").append(timeBlocks);
 };
-
-const renderTimeBlocks = function (timeBlock) {
-  console.log($(timeBlock).parent());
-
-  //map over the timeBlockLabels to construct each TimeBlock
-  timeBlockLabels.map(constructTimeBlock);
-};
-// add a event listener click to save
-$("#time-block-container").on("click", onSave);
 
 const renderCurrentDay = function () {
-  //get current date
-  const date = moment();
-  //format the date
-  const getCurrentDay = function () {
-    currentDay.text(moment().format("dddd MMMM Do YYYY"));
-  };
-  currentDay.text(getCurrentDay);
+  currentDay.text(moment().format("dddd MMMM Do YYYY"));
+};
+
+const setClock = function () {
+  clockContainer.text(moment().format("kk:mm:ss"));
 };
 
 const renderClock = function () {
-  const setClock = function () {
-    //get current time
-    const hourTime = moment();
-    const hourTimeFormatted = hourTime.format("kk:mm:ss");
-
-    clockContainer.text(hourTimeFormatted);
-  };
-  const timer = setInterval(setClock, 1000);
+  setInterval(setClock, 1000);
 };
 
 const onLoad = function () {
   renderCurrentDay();
 
+  setClock();
+
   renderClock();
 
   renderTimeBlocks();
-
-  $(".hour #textarea").val(localStorage.getItem("key"));
 };
 
-$(window).on("load", onLoad);
+const onClear = function () {
+  localStorage.clear();
+
+  $("#time-block-container").empty();
+
+  renderTimeBlocks();
+};
+
+// add a event listener click to save button
+$("#time-block-container").on("click", onSave);
+
+// add a event listener click to clear all button
+$("#clear-all-btn").on("click", onClear);
+
+$(document).ready(onLoad);
